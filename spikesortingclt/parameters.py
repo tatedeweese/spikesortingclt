d8b4571b2075f9dc07356270b218027a88b25272
Tate DeWeese <88437490+tatedeweese@users.noreply.github.com>
2024-02-12 13:26:46 -0500

    updated parametrs

--- spikesortingclt/parameters.py
+++ spikesortingclt/parameters.py
@@ -1,15 +1,24 @@
 import os
 # compiles parameters in one place for subsequent spike sorting
 
-# ecephys_spike_sorting: sglx_multi_run_pipeline.py parameters
+# ----------------------------------
+# SGLX_MULTI_RUN_PIPELINE PARAMETERS
+# ----------------------------------
 logName = "" # Default: runName_log.csv
-npx_directory = r'C:\Users\tdeweese\Documents\test-data' # folder above _g0
-# run_specs = name, gate, trigger and probes to process
-runName = '012223_INS10_DT3_rec' # run_specs, before _g0 in folder name
-run_specs = [[runName, '0', '0,0', '0', ['cortex'] ]]
-catGT_dest = "" # Default: npx_directory/runName_g0_out
+npx_directory = r'C:\Users\tdeweese\Documents\test-data' # folder above _ga (e.g. g0)
+run_specs = [ # run_specs = name, gate, trigger and probes to process
+    ['012223_INS10_DT3_rec', '0', '0,0', '0', ['cortex']]
+]
+# CATGT PARAMETERS
+catGT_dest = "" # Default: npx_directory/runName_ga_out (if multiple runNames will use first)
 run_CatGT = True
+process_lf = False
+catGT_cmd_string = '-prb_fld -out_prb_fld -apfilter=butter,12,300,10000 -lffilter=butter,12,1,500 -gfix=0.4,0.10,0.02 '
+ni_present = True # will validate this
+ni_extract_string = '-xa=0,0,0,1,3,500 -xia=0,0,1,3,3,0 -xd=0,0,-1,1,50 -xid=0,0,-1,2,1.7 -xid=0,0,-1,3,5'
+# TPRIME PARAMETERS
 runTPrime = True
+# MODULES LIST
 modules = [  # comment out undesired modules 
     'kilosort_helper',
     'kilosort_postprocessing',
@@ -19,16 +28,30 @@ modules = [  # comment out undesired modules
 ]
 json_directory = "" # Default: ecephys_spike_sorting/json_files
 
-# burst-detector
+# --------------
+# CLT PARAMETERS
+# --------------
 burst_detector_path = r'C:\Users\tdeweese\Documents\SpikeSorting\burst-detector'
+ecephys_directory = r'C:\Users\tdeweese\Documents\SpikeSorting\ecephys_spike_sorting\ecephys_spike_sorting'
 
 # conda environment names
 ecephys_env = "ece_pyks2"
 phy_env = "phy2"
 burst_env = "burst-detector"
 
+# ------------
+# RUN COMMANDS
+# ------------
+# Note: will always run sglx_multi_run_pipeline.py
+plot_units = True # burst-detector/burst_detector/plot_units.py
+quality_metrics = True # ecephys_spike_sorting.modules.quality_metrics
+custom_metrics = True # burst-detector/burst_detector/custom_metrics.py
+phy = True # startup phy in KS_folder
+
+
 ### DO NOT EDIT BELOW ###
 # Set default names if not defined above
+runName = run_specs[0][0]
 if not catGT_dest:
     catGT_dest = os.path.join(npx_directory, f"{runName}_g0_out")
 if not json_directory:
