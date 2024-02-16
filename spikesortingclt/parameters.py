61e40cfaad4553e720e10b3f0af1ee4be698121e
Tate DeWeese <88437490+tatedeweese@users.noreply.github.com>
2024-02-16 12:42:47 -0500

    Currently getting error in sglx_multi_run_pipeline and quality metrics  using subprocess. issue with marshmellow vars

--- spikesortingclt/parameters.py
+++ spikesortingclt/parameters.py
@@ -5,54 +5,16 @@ import os
 # RUN COMMANDS
 # ------------
 run_pipeline = False # ecephys_spike_sorting/ecephys_spike_sorting/scripts/sglx_multi_run_pipeline.py
-plot_units = True # burst-detector/burst_detector/plot_units.py
-quality_metrics = True # ecephys_spike_sorting.modules.quality_metrics
-custom_metrics = True # burst-detector/burst_detector/custom_metrics.py
+plot_units = False # burst-detector/burst_detector/plot_units.py
+quality_metrics = False # ecephys_spike_sorting.modules.quality_metrics
+custom_metrics = False # burst-detector/burst_detector/custom_metrics.py
 phy = True # startup phy in KS_folder
 
-
-# ----------------------------------
-# SGLX_MULTI_RUN_PIPELINE PARAMETERS
-# ----------------------------------
-logName = "" # Default: runName_log.csv
-npx_directory = r'D:\\' # C:\Users\tdeweese\Documents\test-data' # folder above _ga (e.g. g0)
-run_specs = [ # run_specs = name, gate, trigger and probes to process
-    ['012223_INS10_DT3_rec', '0', '0,0', '0', ['cortex']]
-]
-# CATGT PARAMETERS
-run_CatGT = True
-process_lf = False
-catGT_cmd_string = '-prb_fld -out_prb_fld -apfilter=butter,12,300,10000 -lffilter=butter,12,1,500 -gfix=0.4,0.10,0.02 '
-ni_present = True # will validate this
-ni_extract_string = '-xa=0,0,0,1,3,500 -xia=0,0,1,3,3,0 -xd=0,0,-1,1,50 -xid=0,0,-1,2,1.7 -xid=0,0,-1,3,5'
-# TPRIME PARAMETERS
-runTPrime = True
-# MODULES LIST
-modules = [  # comment out undesired modules 
-    'kilosort_helper',
-    'kilosort_postprocessing',
-    'noise_templates',
-    # 'mean_waveforms',
-    'quality_metrics'
-]
-json_directory = "" # Default: ecephys_spike_sorting/json_files
-
-# --------------
-# CLT PARAMETERS (should only need to set once)
-# --------------
 burst_detector_path = r'C:\Users\tdeweese\Documents\SpikeSorting\burst-detector'
 ecephys_directory = r'C:\Users\tdeweese\Documents\SpikeSorting\ecephys_spike_sorting\ecephys_spike_sorting'
+# conda_path = r"C:\Users\tdeweese\miniconda3"
 
 # conda environment names
 ecephys_env = "ece_pyks2"
 phy_env = "phy2"
 burst_env = "burst-detector"
-
-### DO NOT EDIT BELOW ###
-# Set default names if not defined above
-runName = run_specs[0][0]
-if not json_directory:
-    ecephys_head = os.path.dirname(ecephys_directory)
-    json_directory = os.path.join(ecephys_head, "json_files")
-if not logName:
-    logName = f"{runName}_log.csv"
---
+++ spikesortingclt/run.py
@@ -0,0 +1,97 @@
+
+from parameters import *
+import sys
+sys.path.append(os.path.join(ecephys_directory, "scripts")) # to avoid module not found errors with sglx_multi_run_pipeline import
+from ecephys_spike_sorting.scripts.sglx_multi_run_pipeline import npx_directory, run_specs, json_directory
+import os
+import json
+import subprocess
+
+conda_prompt_path = os.path.join(sys.exec_prefix, "Scripts", "activate.bat") # this is dependent on environment I python run this in
+
+def create_burst_json(filepath, imec_folder, ks_folder, run_str, prb):
+    data_filepath = os.path.join(imec_folder, f"{run_str}_tcat.imec{prb}.ap.bin")
+    burst_dir = {
+        "data_filepath": data_filepath.replace('\\', '/'),
+        "KS_folder": ks_folder.replace('\\', '/')
+    }
+    with open(filepath, "w+") as f:
+        json.dump(burst_dir, f, indent=4)
+
+def execute(cmd, conda=True):
+    if conda:
+        cmd = f"call {conda_prompt_path} && {cmd}"
+    with subprocess.Popen(cmd.split(), shell=True, bufsize=1, stdout=subprocess.PIPE, universal_newlines=True) as popen:
+        for stdout_line in popen.stdout:
+            print(stdout_line, end="")
+    if popen.returncode != 0:
+        raise subprocess.CalledProcessError(popen.returncode, popen.args)
+    
+def run_subprocesses(burst_input_json_path, burst_output_json_path, quality_input_json, quality_output_json, ks_folder):
+    sglx_file = os.path.join(ecephys_directory, "scripts", "sglx_multi_run_pipeline.py")
+    plot_units_file = os.path.join(burst_detector_path, "burst_detector", "plot_units.py")
+    custom_metrics_file = os.path.join(burst_detector_path, "burst_detector", "custom_metrics.py")
+    phy_file = os.path.join(ks_folder, "params.py")
+    execute("ls")
+    if run_pipeline:
+        print("~~~RUNNING SGLX_MULTI_RUN_PIPELINE~~~")
+        conda_cmd = f"conda activate {ecephys_env}"
+        python_cmd = f"python {sglx_file}"
+        print(conda_cmd)
+        print(python_cmd)
+        execute(f"{conda_cmd} && {python_cmd}")
+    if plot_units:
+        print("~~~RUNNING PLOT_UNITS~~~")
+        conda_cmd = f"conda activate {burst_env}"
+        python_cmd = f"python {plot_units_file} --input {burst_input_json_path} --output {burst_output_json_path}"
+        print(conda_cmd)
+        print(python_cmd)
+        execute(f"{conda_cmd} && {python_cmd}")
+    if quality_metrics:
+        print("~~~RUNNING QUALITY_METRICS~~~")
+        conda_cmd = f"conda activate {ecephys_env}"
+        python_cmd = f"python -m ecephys_spike_sorting.modules.quality_metrics --input {quality_input_json} --output {quality_output_json}"
+        print(conda_cmd)
+        print(python_cmd)
+        execute(f"{conda_cmd} && {python_cmd}")
+    if custom_metrics:
+        print("~~~RUNNING CUSTOM_METRICS~~~")
+        conda_cmd = f"conda activate {burst_env}"
+        python_cmd = f"python {custom_metrics_file}"
+        print(conda_cmd)
+        print(python_cmd)
+        execute(f"{conda_cmd} && {python_cmd}")
+    if phy:
+        print("~~~RUNNING PHY~~~")
+        conda_cmd = f"conda activate {phy_env}"
+        phy_cmd = f"phy template-gui {phy_file}"
+        print(conda_cmd)
+        print(phy_cmd)
+        execute(f"{conda_cmd} && {phy_cmd}")
+    
+def main():
+    prb = 0
+    gate = 0
+    runName = run_specs[0][0]
+    run_str = f"{runName}_g{gate}"
+    prb_str = f"{run_str}_imec{prb}"
+    catgt_folder = os.path.join(npx_directory, run_str, f"catgt_{run_str}")
+
+    # create json
+    burst_input_json_path = os.path.join(burst_detector_path, "json_files", f"{run_str}.json")    
+    burst_output_json_path = os.path.join(burst_detector_path, "json_files", f"{run_str}-output.json")
+    imec_folder = os.path.join(catgt_folder, prb_str)
+    ks_folder = os.path.join(imec_folder, f"imec{prb}_ks2")
+    create_burst_json(burst_input_json_path, imec_folder, ks_folder, run_str, prb)
+
+    # ecephys: quality_metrics
+    quality_input_json = os.path.join(json_directory, f"{runName}_imec{prb}-input.json")
+    quality_output_json = os.path.join(json_directory, f"{runName}_imec{prb}-output.json")
+ 
+    run_subprocesses(burst_input_json_path, burst_output_json_path, quality_input_json, quality_output_json, ks_folder)
+
+    
+if __name__ == "__main__":
+    main()
+
+
